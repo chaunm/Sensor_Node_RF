92c55bc196c0ea727c6cd00c6cd1996bcb24bafd
chaunm <chau-nm@hotmail.com>
2019-10-09 08:11:00 +0700

    sht3x implementation

--- Driver/sensor/sht3x.h
+++ Driver/sensor/sht3x.h
@@ -1,11 +1,7 @@
 #ifndef __SHT3X_H__
 #define __SHT3X_H__
 
-                                  //adr  command  r/w
-#define SHT1_STATUS_REG_W 0x06   //000   0011    0
-#define SHT1_STATUS_REG_R 0x07   //000   0011    1
-#define SHT1_MEASURE_TEMP 0x03   //000   0001    1
-#define SHT1_MEASURE_HUMI 0x05   //000   0010    1
-#define SHT1_RESET        0x1e   //000   1111    0
+#define SHT3X_FLAG_TIMEOUT         ((uint32_t)0x1000)
+#define SHT3X_LONG_TIMEOUT         ((uint32_t)(10 * SHT3X_FLAG_TIMEOUT)) 
 
 #endif
